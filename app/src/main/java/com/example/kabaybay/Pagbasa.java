0b8e1d9bf22ac51f6d39d4041449809d16f2ecc2
Gagocode <96364834+Gagocode@users.noreply.github.com>
2023-04-01 21:38:57 +0800

    new activities

---
+++ app/src/main/java/com/example/kabaybay/Pagbasa.java
@@ -0,0 +1,22 @@
+package com.example.kabaybay;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class Pagbasa extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pagbasa);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getSupportActionBar().hide();
+        this.getWindow().setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+        );
+    }
+}
