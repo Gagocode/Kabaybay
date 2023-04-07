197ac8f12716af74dde16a0fca612a617e8c7b31
Gagocode <96364834+Gagocode@users.noreply.github.com>
2023-04-07 21:12:07 +0800

    trying tp kill my self hehe

--- app/src/main/java/com/example/kabaybay/Kasaysayan.java
+++ app/src/main/java/com/example/kabaybay/Kasaysayan.java
@@ -1,18 +1,11 @@
 package com.example.kabaybay;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle ;
-import android.view.GestureDetector;
-import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
-
-import kotlin.math.UMathKt;
 
 public class Kasaysayan extends AppCompatActivity {
 
@@ -32,11 +25,34 @@ public class Kasaysayan extends AppCompatActivity {
                 openSimula();
             }
         });
+        btn2 = (Button) findViewById(R.id.button2);
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openKolonyalisasyon();
+            }
+        });
+        btn3 = (Button) findViewById(R.id.button3);
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openRebolusyon();
+            }
+        });
     }
 
     private void openSimula() {
         Intent intent = new Intent(this, Simula.class);
         startActivity(intent);
     }
+    private void openKolonyalisasyon() {
+        Intent intent = new Intent(this, Koloniyalisasyon.class);
+        startActivity(intent);
+    }
+    private void openRebolusyon() {
+        Intent intent = new Intent(this, Rebolusyon.class);
+        startActivity(intent);
+    }
+
 
 }
--- app/src/main/java/com/example/kabaybay/Pagbasa.java
+++ app/src/main/java/com/example/kabaybay/Pagbasa.java
@@ -2,61 +2,45 @@ package com.example.kabaybay;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
 import android.widget.Button;
 
 public class Pagbasa extends AppCompatActivity {
-
     private Button btn;
     private Button btn2;
-    private Button btn3;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pagbasa);
 
-        btn = (Button) findViewById(R.id.button5);
-        btn2 = (Button) findViewById(R.id.button6);
-        btn3 = (Button) findViewById(R.id.button7);
+        btn = (Button) findViewById(R.id.patinig);
+        btn2 = (Button) findViewById(R.id.katinig);
 
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openBa();
+                openPatinig();
             }
         });
         btn2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openKa();
+                openKatinig();
             }
         });
-        btn3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openTa();
-            }
-        });
-
     }
 
-    private void openTa() {
-        Intent intent = new Intent(this, Ta.class);
+    private void openKatinig() {
+        Intent intent = new Intent(this, Katinig.class);
         startActivity(intent);
     }
 
-    private void openKa() {
-        Intent intent = new Intent(this, Ka.class);
-        startActivity(intent);
-    }
-
-    private void openBa() {
-        Intent intent = new Intent(this, Ba.class);
+    private void openPatinig() {
+        Intent intent = new Intent(this, Patinig.class);
         startActivity(intent);
     }
 }
