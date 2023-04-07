4042b4e5206c1257b47aa06eb27b7fe9710c65a9
Gagocode <96364834+Gagocode@users.noreply.github.com>
2023-04-07 17:19:03 +0800

    me dead

--- app/src/main/java/com/example/kabaybay/Kasaysayan.java
+++ app/src/main/java/com/example/kabaybay/Kasaysayan.java
@@ -3,10 +3,12 @@ package com.example.kabaybay;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle ;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
@@ -14,11 +16,27 @@ import kotlin.math.UMathKt;
 
 public class Kasaysayan extends AppCompatActivity {
 
+    private Button btn;
+    private Button btn2;
+    private Button btn3;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_kasaysayan);
 
+        btn = (Button) findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openSimula();
+            }
+        });
+    }
+
+    private void openSimula() {
+        Intent intent = new Intent(this, Simula.class);
+        startActivity(intent);
     }
 
 }
