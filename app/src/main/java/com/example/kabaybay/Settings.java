ad87916e3a8f6a7573af994de8e1436e02bf9613
Gagocode <96364834+Gagocode@users.noreply.github.com>
2023-05-14 14:36:15 +0800

    setting settings

--- app/src/main/java/com/example/kabaybay/Settings.java
+++ app/src/main/java/com/example/kabaybay/Settings.java
@@ -1,14 +1,73 @@
 package com.example.kabaybay;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class Settings extends AppCompatActivity {
 
+    private ImageButton themes, lang, info, back;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        themes = (ImageButton) findViewById(R.id.themes);
+        themes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                fragmentManager.beginTransaction()
+                        .replace(R.id.fragmentContainerView2, Themes.class, null)
+                        .setReorderingAllowed(true)
+                        .addToBackStack("") // Name can be null
+                        .commit();
+            }
+        });
+
+        lang = (ImageButton) findViewById(R.id.lang);
+        lang.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                fragmentManager.beginTransaction()
+                        .replace(R.id.fragmentContainerView2, Language.class, null)
+                        .setReorderingAllowed(true)
+                        .addToBackStack("") // Name can be null
+                        .commit();
+            }
+        });
+
+        info = (ImageButton) findViewById(R.id.info);
+        info.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                fragmentManager.beginTransaction()
+                        .replace(R.id.fragmentContainerView2, Info.class, null)
+                        .setReorderingAllowed(true)
+                        .addToBackStack("") // Name can be null
+                        .commit();
+            }
+        });
+
+        back = (ImageButton) findViewById(R.id.back);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goBack();
+            }
+        });
+
+    }
+
+    private void goBack() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
